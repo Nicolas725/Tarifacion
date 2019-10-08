8a84d33594670a2c4538acb3b89d7c16b9d369cb
Nicolas725 <nicolas@zoho.com>
2019-10-08 10:52:53 -0300

    carga

--- tabla_salientes.php
+++ tabla_salientes.php
@@ -1,72 +1,101 @@
 <?php
 	include("./conexion.php");
+?>
 
- ?>
-<div class="row">
-	<div class="col-sm-12">
-	<h2>LLamadas Salientes</h2>
-		<table class="table table-hover table-condensed table-bordered">
-			<tr>
-			</tr>
-			<tr>
-			</tr>
-			<tr>
-				<td>Interno</td>
-        <td>Nombre</td>
-				<td>Fecha</td>
-				<td>Hora</td>
-				<td>Duration</td>
-				<td>Destino</td>
-				<td>Tipo</td>
-			</tr>
+<html>
+	<body>
 
-			<?php
-				$sql="(SELECT
-										chargeduserid,
-										suscribername,
-										date,
-										time,
-										callduration,
-										diallednumber,
-										communicationtype
+		<form method="post" action="process_salientes.php">
+			<br/>
+			Seleccione la opcion por la cual desea filtrar:
+			<br/>
+			<input type="checkbox" name="inter1" value="inter1">Interno<br>
+			<input type="checkbox" name="date" value="date">Fecha<br>
+			<input type="checkbox" name="sed" value="sed">Sede<br>
+			<input type="checkbox" name="depar" value="depar">Departamento<br>
+		  <input type="submit" value="Filtrar">
+		</form>
 
-										FROM
+	<div class="row">
+		<div class="col-sm-12">
+		<h2>LLamadas Salientes</h2>
+			<table class="table table-hover table-condensed table-bordered">
+				<tr>
+					<td>Interno</td>
+	        <td>Nombre</td>
+					<td>Fecha</td>
+					<td>Hora</td>
+					<td>Duration</td>
+					<td>Destino</td>
+					<td>Tipo</td>
+					<td>Sede</td>
+					<td>Departamento</td>
+				</tr>
 
-												tickets_outgoing
-												)
-										UNION
+				<?php
+				/*if(!isset($_POST['frmSearch'])){
+					$opcion = $_POST['frmSearch'];
+					echo $opcion;
+					echo "estoy aca";
+				}*/
+				/*if(!isset($_POST['dateFrom'])){
+					$new_date = date('Y-m-d', strtotime($_POST['dateFrom']));
+					echo $new_date;
+				}*/
 
-							(SELECT
-										chargeduserid,
-										suscribername,
-										date,
-										time,
-										callduration,
-										diallednumber,
-										communicationtype
+					$sql="(SELECT
+											chargeduserid,
+											suscribername,
+											date,
+											time,
+											callduration,
+											diallednumber,
+											communicationtype,
+											nombreSede,
+											nombreDepar
 
-										FROM
+											FROM
 
-												tickets_outgoing_transfer)
-									ORDER BY date,time DESC";
+													tickets_outgoing
+													)
+											UNION
 
-				$result=mysqli_query($conexion,$sql);
-				while($ver=mysqli_fetch_row($result)){
-			 ?>
+								(SELECT
+											chargeduserid,
+											suscribername,
+											date,
+											time,
+											callduration,
+											diallednumber,
+											communicationtype,
+											nombreSede,
+											nombreDepar
 
-			<tr>
-				<td><?php echo $ver[0] ?></td>
-				<td><?php echo $ver[1] ?></td>
-				<td><?php echo $ver[2] ?></td>
-				<td><?php echo $ver[3] ?></td>
-				<td><?php echo $ver[4] ?></td>
-				<td><?php echo $ver[5] ?></td>
-				<td><?php echo $ver[6] ?></td>
+											FROM
 
-			</tr>
-			<?php
-		}
-			 ?>
-		</table>
+													tickets_outgoing_transfer)
+										";
+
+					$result=mysqli_query($conexion,$sql);
+					while($ver=mysqli_fetch_row($result)){
+				 ?>
+
+				<tr>
+					<td><?php echo $ver[0] ?></td>
+					<td><?php echo $ver[1] ?></td>
+					<td><?php echo $ver[2] ?></td>
+					<td><?php echo $ver[3] ?></td>
+					<td><?php echo $ver[4] ?></td>
+					<td><?php echo $ver[5] ?></td>
+					<td><?php echo $ver[6] ?></td>
+					<td><?php echo $ver[7] ?></td>
+					<td><?php echo $ver[8] ?></td>
+
+				</tr>
+				<?php
+			}
+				 ?>
+			</table>
+		</div>
 	</div>
-</div>
+</body></html>
