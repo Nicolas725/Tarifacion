cb88072681639d2f317fe7b8b7179e79fdbb10f8
Nicolas <nico.collovati@gmail.com>
2021-06-27 16:24:19 -0300

    Correcion importador para git bash sh

--- scripts/importador/importador_tickets.sh
+++ scripts/importador/importador_tickets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 db_user="root"
-db_pass="mac1778##"
+db_pass=""
 db_server="localhost"
 db_name="telefonos"
 
@@ -9,17 +9,18 @@ db_name="telefonos"
 #mount /home/martin/Escritorio/Importadores_cron/Tickets /mnt/tickets/
 
 # Eliminar los tickets viejos
-rm -vRf C:\xampp\htdocs\www\TESIS\Tarifacion\scripts\Tickets\*
+rm -vRf /c/xampp/htdocs/Tarifacio/scripts/Tickets/
+
 # Copiamos los nuevos tickets
-cp -v C:\Users\marti\Desktop\Tickets\*.xml C:\xampp\htdocs\www\TESIS\Tarifacion\scripts\Tickets\*
+cp -v /c/Users/nico_/Desktop/Tickets/*.xml /c/xampp/htdocs/Tarifacion/scripts/Tickets/
 # Limpio las tablas de la DB
 #mysql -u $db_user -p$db_pass -e "USE $db_name; TRUNCATE TABLE tickets_outgoing; TRUNCATE TABLE tickets_outgoing_transfer;"
 #mysql -u $db_user -p$db_pass -e "USE $db_name; TRUNCATE TABLE tickets_incoming; TRUNCATE TABLE tickets_incoming_transfer;"
 # Importamos los nuevos tickets
-for x in C:\xampp\htdocs\www\TESIS\Tarifacion\scripts\Tickets\*.xml
+for x in /c/xampp/htdocs/Tarifacion/scripts/Tickets/*.xml
 do
 	echo Importando: $x
-	python C:\xampp\htdocs\www\TESIS\Tarifacion\scripts\alca-mysql.py $x
+	python /c/xampp/htdocs/Tarifacion/scripts/alca-mysql.py $x
 done
 mysql -u $db_user -p$db_pass -e "USE $db_name; DELETE FROM tickets_outgoing WHERE id='1'; DELETE FROM tickets_outgoing WHERE id='2';"
 
